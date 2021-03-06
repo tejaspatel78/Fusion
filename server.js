cc436837e7e32b541beb5e9ef42e402faa94b693
tejaspatel78 <tejaspatel686@gmail.com>
2021-03-06 15:42:43 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,14 @@
+const exp = require('express');
+const app = express();
+
+//  Default routes
+app.get('/', (req, res) => {
+    res.send('Hello world - My Demo Project');
+});
+
+//  Listening Server
+app.listen(3030, async () => {
+    console.log(`*****************************************************************************`);
+    console.log(`Server running at http://${host}`);
+    console.log(`*****************************************************************************`);
+});
